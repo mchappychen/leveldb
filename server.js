7a8f4acba6238f6cd3a9f2c241de3bd6edacbcf3
Michael <51720724+mchappychen@users.noreply.github.com>
2019-06-24 14:56:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 var levelup = require('levelup')
 var leveldown = require('leveldown')
 
-// 1) Create our store
+// 1) Create our database
 var db = levelup(leveldown("./mydb"))
 
-// 2) Put a key & value
+// 2) Put in a key and value
 db.put("A", 123, function (err) {
-  if (err) return console.log('Ooops!', err) // some kind of I/O error
 
-  // 3) Fetch by key
+  if(err){return console.log("Error: ",error);}
+
+  // 3) Get the key's value
   db.get("A", function (err, value) {
-    if (err) return console.log('Ooops!', err) // likely the key was not found
-
-    // Ta da!
-    console.log('name=' + value)
+    if(err){return console.log("Error: ",error);}
+    
+    console.log('' + value) //FOR SOME REASON, YOU NEED A STRING+VALUE OR ELSE IT RETURNS A <BUFFER> THING
   })
 })
